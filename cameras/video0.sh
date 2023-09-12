eba733fe506d858a6ac84c6abf06f26db92abc74
Maciej Malczyk <wujekbrezniew@gmail.com>
2023-09-12 14:04:36 +0200

    Fixed install script

--- cameras/video0.sh
+++ cameras/video0.sh
@@ -4,4 +4,4 @@ image="video0_$(date +%Y%m%d_%H%M%S).jpg"
 
 fswebcam -d /dev/video0 -r 2592x1944 --no-banner $image
 
-sftp -i ~/.ssh/id_rsa -P 8022 img@helmholtz.polsl.pl:/images <<< $'put '$image
+sftp -i ~/.ssh/img -P 8022 img@helmholtz.polsl.pl:/images <<< $'put '$image
--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-echo "1: Update python setuptools"
+echo "1: Install pip3"
+sudo apt update && sudo apt install -y python3-pip fswebcam
+echo "2: Update python setuptools"
 sudo pip3 install --upgrade setuptools
-echo "2: Install raspi-blinka"
+echo "3: Install raspi-blinka"
 sudo pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo python3 raspi-blinka.py
-echo "3: Install libraries for sensors"
+echo "4: Install libraries for sensors"
 pip3 install adafruit-circuitpython-lis3dh
 pip3 install adafruit-circuitpython-mcp9808
-pip3 install asyncio websockets ADS1x15-ADC
+pip3 install asyncio websockets ADS1x15-ADC smbus pymongo
 cd ./modules
 git clone https://github.com/wujekbrezniew/python-apds9960
 cp -r python-apds9960/apds9960 apds9960
 sudo rm -r python-apds9960
+echo "5: Generate ssh keys"
+echo "/home/$USER/.ssh/img" | ssh-keygen
+ssh-copy-id -i ~/.ssh/img -p 8022 img@192.168.1.102
+echo "6: Setup i2c-0"
+sudo su -c "echo dtparam=i2c_vc=on >> /boot/config.txt"
 sudo reboot

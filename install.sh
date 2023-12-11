7553ab6a0c013ed612ff0421489bf4b11ec46b5a
Maciej Malczyk <wujekbrezniew@gmail.com>
2023-12-11 14:31:25 +0100

    Add config for disabling WiFi energy savings for RPIZero

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "1: Install pip3"
-sudo apt update && sudo apt install -y python3-pip python3-opencv
+sudo apt update && sudo apt install -y python3-pip python3-opencv git
 echo "2: Update python setuptools"
 sudo pip3 install --upgrade setuptools
 echo "3: Install raspi-blinka"
@@ -16,9 +16,12 @@ cd ./modules
 git clone https://github.com/wujekbrezniew/python-apds9960
 cp -r python-apds9960/apds9960 apds9960
 sudo rm -r python-apds9960
+cd ..
 echo "5: Generate ssh keys"
 echo "/home/$USER/.ssh/img" | ssh-keygen
 ssh-copy-id -i ~/.ssh/img -p 8022 img@golfserver.local
 echo "6: Setup i2c-0"
 sudo su -c "echo dtparam=i2c_vc=on >> /boot/config.txt"
+echo "7: Disable wifi power saving"
+sudo cp ./misc/rc.local /etc/
 sudo reboot

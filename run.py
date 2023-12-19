486c32a8b73c2e76dad13bdfbc3c10aea83c919a
Maciej Malczyk <wujekbrezniew@gmail.com>
2023-12-19 15:45:53 +0100

    Change multithreading to threads in run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from modules import *
-from multiprocessing import Process
+import threading
 import time
 import sys
 import signal
@@ -10,57 +10,40 @@ def sigterm_handler(signal, frame):
     print("Killing processes")
     global semaphore
     semaphore = False
-    print("P1 kill")
-    p1.kill()
-    p1.close()
-    print("P2 kill")
-    p2.kill()
-    p2.close()
-    print("P3 kill")
-    p3.kill()
-    p3.close()
-    print("Pcam kill")
-    pcam.kill()
-    pcam.close()
-    print("Exit")
     sys.exit()
 
 
 signal.signal(signal.SIGTERM, sigterm_handler)
 
-def task1():
+def thread_light_temp():
     while semaphore:
         light_temperature.send()
         time.sleep(120)
 
-def task2():
+def thread_acc():
     while semaphore:
         acceleration.send()
         time.sleep(150/1000)
 
-def task3():
+def thread_water():
     while semaphore:
         watering.send()
         time.sleep(60)
 
-def task_cameras():
+def thread_cameras():
     while semaphore:
         cameras.send()
         time.sleep(600)
 
 try:
-    p1 = Process(target=task1)
-    p2 = Process(target=task2)
-    p3 = Process(target=task3)
-    pcam = Process(target=task_cameras)
-    p1.start()
-    p1.join()
-    p2.start()
-    p2.join()
-    p3.start()
-    p3.join()
-    pcam.start()
-    pcam.join()
+    th_wt = threading.Thread(target=thread_light_temp, args=(1,))
+    th_acc = threading.Thread(target=thread_acc, args=(1,))
+    th_w = threading.Thread(target=thread_water, args=(1,))
+    th_cam = threading.Thread(target=thread_cameras, args=(1,))
+    th_wt.start()
+    th_acc.start()
+    th_w.start()
+    th_cam.start()
 
 except:
     print("Exiting...")

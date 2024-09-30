198e38cf04ad1ef1ba931f643f15c9d85dc4c4e3
mikoma <wujekbrezniew@gmail.com>
2024-09-30 12:59:35 +0200

    Fix __init__.py

--- modules/sensors/__init__.py
+++ modules/sensors/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["ADS1x15", "apds9960", "ambient_light_i2c0", "ambient_light_i2c1", "accel", "temperature_0", "temperature_1", "moisture"]
+__all__ = ["ADS1x15", "apds9960", "ambient_light", "accel", "temperature", "moisture"]

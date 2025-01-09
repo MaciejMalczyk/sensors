72dc365096b09409fe28e6ec485949e44e485ff7
mikoma <wujekbrezniew@gmail.com>
2025-01-09 13:37:19 +0100

    Add check in install.sh about rpi to rule out unnecessary comands

--- install.sh
+++ install.sh
@@ -1,44 +1,69 @@
 #!/bin/bash
 
 STEP=0
+RPI=0
+BL='\033[1;34m'
+NC='\033[0m'
 
-echo "$STEP: Checking connection to server"
+echo -e "${BL} $STEP: Checking connection to server ${NC}"
 
 if ping -c 1 clinostate.server &> /dev/null ; then
-    echo "Continue..."
+    echo -e "Continue..."
 else
-    echo "No connection to clinostate.server. Setup VPN and add address to /etc/hosts"
+    echo -e "No connection to clinostate.server. Setup VPN and add address to /etc/hosts"
     exit 0
 fi
 
-((STEP++)); echo "$STEP: Enralge swap to 2G"
-sudo dphys-swapfile swapoff
-sudo cp ./misc/dphys-swapfile /etc
-sudo dphys-swapfile setup
-sudo dphys-swapfile swapon
-((STEP++)); echo "$STEP: Install pip3 and packages"
+if command -v raspi-config >/dev/null 2>&1
+then
+    echo -e "${BL}Raspberry Pi device detected.${NC}"
+    RPI=1
+else
+    echo -e "${BL}Other device detected.${NC}"
+fi
+
+if [ $RPI -eq 1 ]
+then
+    ((STEP++)); echo -e "${BL} $STEP: Enralge swap to 2G ${NC}"
+    sudo dphys-swapfile swapoff
+    sudo cp ./misc/dphys-swapfile /etc
+    sudo dphys-swapfile setup
+    sudo dphys-swapfile swapon
+fi
+
+((STEP++)); echo -e "${BL} $STEP: Install pip3 and packages ${NC}"
 sudo apt update && sudo apt install -y python3-pip python3-opencv python3-fabric v4l-utils
-((STEP++)); echo "$STEP: Update python setuptools"
+
+((STEP++)); echo -e "${BL} $STEP: Update python setuptools ${NC}"
 sudo pip3 install --upgrade setuptools --break-system-packages
-((STEP++)); echo "$STEP: Install libraries for sensors"
-pip3 install asyncio websockets ADS1x15-ADC smbus pymongo websockets --break-system-packages
+
+((STEP++)); echo -e "${BL} $STEP: Install libraries for sensors ${NC}"
+pip3 install asyncio websockets ADS1x15-ADC smbus2 pymongo websockets --break-system-packages
 cd ./modules || exit 1
 git clone https://github.com/wujekbrezniew/python-apds9960
 cp -r python-apds9960/apds9960 apds9960
 sudo rm -r python-apds9960
 cd .. || exit 1
-((STEP++)); echo "$STEP: Generate ssh keys"
-echo "/home/$USER/.ssh/img" | ssh-keygen
+
+((STEP++)); echo -e "${BL} $STEP: Generate ssh keys ${NC}"
+echo -e "/home/$USER/.ssh/img" | ssh-keygen -q -N ""
 ssh-copy-id -i ~/.ssh/img -p 8022 img@clinostate.server
-((STEP++)); echo "$STEP: Setup i2c-0"
-sudo su -c "echo dtparam=i2c_vc=on >> /boot/firmware/config.txt"
-sudo raspi-config nonint do_i2c 0
-((STEP++)); echo "$STEP: Disable wifi power saving"
-sudo cp ./misc/rc.local /etc/
-((STEP++)); echo "$STEP: Install sensors service"
+
+if [ $RPI -eq 1 ]
+then
+    ((STEP++)); echo -e "${BL} $STEP: Setup i2c-0 ${NC}"
+    sudo su -c "echo -e dtparam=i2c_vc=on >> /boot/firmware/config.txt"
+    sudo raspi-config nonint do_i2c 0
+
+    ((STEP++)); echo -e "${BL} $STEP: Disable wifi power saving ${NC}"
+    sudo cp ./misc/rc.local /etc/
+fi
+
+((STEP++)); echo -e "${BL} $STEP: Install sensors service ${NC}"
 sudo cp ./misc/sensors.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable sensors.service
-((STEP++)); echo "$STEP: Reboot: 3s"
+
+((STEP++)); echo -e "${BL} $STEP: Reboot: 3s ${NC}"
 sleep 3s
 sudo reboot
